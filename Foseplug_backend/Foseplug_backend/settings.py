b0a0583df07247893a8059a3ee66db46d54f9348
ackahkelvin45 <ackahkelvin455@gmail.com>
2023-09-04 20:53:19 +0000

    third commit

--- Foseplug_backend/Foseplug_backend/settings.py
+++ Foseplug_backend/Foseplug_backend/settings.py
@@ -230,6 +230,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/images/'
 
 # Default primary key field type
